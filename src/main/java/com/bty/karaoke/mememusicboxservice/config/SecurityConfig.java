aa51cebdd4b308511eb694f8cade6e973449bea3
BTY <phuocpham044@gmail.com>
2026-05-29 02:58:24 +0700

    add: CORS config

--- src/main/java/com/bty/karaoke/mememusicboxservice/config/SecurityConfig.java
+++ src/main/java/com/bty/karaoke/mememusicboxservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,7 +44,7 @@ public class SecurityConfig {
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter))
                         .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
                 )
-
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
         ;
         return httpSecurity.build();
